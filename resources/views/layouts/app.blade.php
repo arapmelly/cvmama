4901cdeb88d3555f4fae1029f82272df8dc8d03f
jacob <jacob.chumo@squaddigital.com>
2021-07-14 18:29:51 +0300

    setup tailwind css

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+  <head>
+    <!-- ... --->
+   <meta charset="UTF-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    @livewireStyles
+  </head>
+  <body>
+
+
+    @livewireScripts
+  </body>
+
+</html>
